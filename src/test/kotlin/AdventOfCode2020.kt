656a0ea146c57f101bfd295bf91b14bf54d856de
Charles Lechasseur <charles.lechasseur@ingeno.ca>
2020-12-02 20:02:20 -0500

    Day 1

--- src/test/kotlin/AdventOfCode2020.kt
+++ src/test/kotlin/AdventOfCode2020.kt
@@ -6,7 +6,12 @@ class AdventOfCode2020 {
     class Day1Puzzles {
         @Test
         fun `day 1, part 1`() {
-            TODO()
+            assertEquals(1_010_299, Day1.part1())
+        }
+
+        @Test
+        fun `day 1, part 2`() {
+            assertEquals(42_140_160, Day1.part2())
         }
     }
 }
